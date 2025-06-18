08019ed2a2fee8afd9fb06f83ab40adaa18b63bd
evilbob27 <evilbob27@ymail.com>
2025-06-18 13:16:10 -0700

    fixed bug in which clicking a button on the menubar would prematurely format the data

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -16,6 +16,7 @@ const MenuBar = ({ editor }: MenuBarProps) => {
             <div
                 className="flex justify-start items-center gap-6 ml-4 *:border *:border-gray-200 *:rounded-md *:px-2 *:hover:bg-gray-200">
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().toggleBold().run()}
                     disabled={
                         !editor.can()
@@ -29,6 +30,7 @@ const MenuBar = ({ editor }: MenuBarProps) => {
                     <span className="font-semibold">Bold</span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().toggleItalic().run()}
                     disabled={
                         !editor.can()
@@ -42,18 +44,21 @@ const MenuBar = ({ editor }: MenuBarProps) => {
                     <span className="italic">Italic</span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().setColor('#000000').run()}
                     className={`flex items-center py-1 ${editor.isActive('textStyle', {color: '#000000'}) ? 'is-active' : ''}`}
                 >
                     <span className="inline-block mx-1 w-4 h-4 rounded-sm bg-black items-center"></span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().setColor('#FF0000').run()}
                     className={`flex items-center py-1 ${editor.isActive('textStyle', {color: '#FF0000'}) ? 'is-active' : ''}`}
                 >
                     <span className="inline-block mx-1 w-4 h-4 rounded-sm bg-[#FF0000]"></span>
                 </button>
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().undo().run()}
                     disabled={
                         !editor.can()
@@ -66,6 +71,7 @@ const MenuBar = ({ editor }: MenuBarProps) => {
                     Undo
                 </button>
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().redo().run()}
                     disabled={
                         !editor.can()
